bf1d44feb27450cc981cdd86b55f55fbc4d49847
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2024-10-25 07:53:53 +0300

    ParseVector2

--- CSharp/Client/CrabUI/CUIExtensions.cs
+++ CSharp/Client/CrabUI/CUIExtensions.cs
@@ -20,6 +20,23 @@ namespace CrabUI
     public static string ParseString(string s) => s; // BaroDev (wide)
     public static GUISoundType ParseGUISoundType(string s) => Enum.Parse<GUISoundType>(s);
     public static Color ParseColor(string s) => XMLExtensions.ParseColor(s);
+    public static Vector2 ParseVector2(string s)
+    {
+      int ix = s.IndexOf("X:") + 2;
+      int iy = s.IndexOf("Y:") + 2;
+      int ib = s.IndexOf("}");
+
+      string sx = s.Substring(ix, iy - ix - 2);
+      string sy = s.Substring(iy, ib - iy);
+
+      float x = 0;
+      float y = 0;
+
+      float.TryParse(sx, out x);
+      float.TryParse(sy, out y);
+
+      return new Vector2(x, y);
+    }
 
     public static Dictionary<Type, MethodInfo> Parse;
     public static Dictionary<Type, MethodInfo> CustomToString;
@@ -32,6 +49,7 @@ namespace CrabUI
       Parse[typeof(string)] = typeof(CUIExtensions).GetMethod("ParseString");
       Parse[typeof(GUISoundType)] = typeof(CUIExtensions).GetMethod("ParseGUISoundType");
       Parse[typeof(Color)] = typeof(CUIExtensions).GetMethod("ParseColor");
+      Parse[typeof(Vector2)] = typeof(CUIExtensions).GetMethod("ParseVector2");
     }
 
   }
