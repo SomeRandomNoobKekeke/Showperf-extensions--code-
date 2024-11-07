4704f80cba135a7fe617cf9c402ea498a5700944
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2024-11-07 21:42:32 +0300

    LevelObject_Update_Replace LevelObjectManager_Update_Replace

---
+++ CSharp/Client/Patches/Capture/Map/Levels/LevelObjects/LevelObject.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Reflection;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+
+using Barotrauma;
+using HarmonyLib;
+
+using Barotrauma.Lights;
+using Barotrauma.Networking;
+using Barotrauma.Particles;
+using Barotrauma.Sounds;
+using Barotrauma.SpriteDeformations;
+using FarseerPhysics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace ShowPerfExtensions
+{
+  public partial class Plugin
+  {
+    [ShowperfPatch]
+    public class LevelObjectPatch
+    {
+      public static void Initialize()
+      {
+        harmony.Patch(
+          original: typeof(LevelObject).GetMethod("Update", AccessTools.all),
+          prefix: new HarmonyMethod(typeof(LevelObjectPatch).GetMethod("LevelObject_Update_Replace"))
+        );
+      }
+
+      public static bool LevelObject_Update_Replace(float deltaTime, LevelObject __instance)
+      {
+        LevelObject _ = __instance;
+
+        _.CurrentRotation = _.Rotation;
+        if (_.ActivePrefab.SwingFrequency > 0.0f)
+        {
+          _.SwingTimer += deltaTime * _.ActivePrefab.SwingFrequency;
+          _.SwingTimer = _.SwingTimer % MathHelper.TwoPi;
+          //lerp the swing amount to the correct value to prevent it from abruptly changing to a different value
+          //when a trigger changes the swing amoung
+          _.CurrentSwingAmount = MathHelper.Lerp(_.CurrentSwingAmount, _.ActivePrefab.SwingAmountRad, deltaTime * 10.0f);
+
+          if (_.ActivePrefab.SwingAmountRad > 0.0f)
+          {
+            _.CurrentRotation += (float)Math.Sin(_.SwingTimer) * _.CurrentSwingAmount;
+          }
+        }
+
+        _.CurrentScale = Vector2.One * _.Scale;
+        if (_.ActivePrefab.ScaleOscillationFrequency > 0.0f)
+        {
+          _.ScaleOscillateTimer += deltaTime * _.ActivePrefab.ScaleOscillationFrequency;
+          _.ScaleOscillateTimer = _.ScaleOscillateTimer % MathHelper.TwoPi;
+          _.CurrentScaleOscillation = Vector2.Lerp(_.CurrentScaleOscillation, _.ActivePrefab.ScaleOscillation, deltaTime * 10.0f);
+
+          float sin = (float)Math.Sin(_.ScaleOscillateTimer);
+          _.CurrentScale *= new Vector2(
+              1.0f + sin * _.CurrentScaleOscillation.X,
+              1.0f + sin * _.CurrentScaleOscillation.Y);
+        }
+
+        if (_.LightSources != null)
+        {
+          for (int i = 0; i < _.LightSources.Length; i++)
+          {
+            if (_.LightSourceTriggers[i] != null) _.LightSources[i].Enabled = _.LightSourceTriggers[i].IsTriggered;
+            _.LightSources[i].Rotation = -_.CurrentRotation;
+            _.LightSources[i].SpriteScale = _.CurrentScale;
+          }
+        }
+
+        if (_.spriteDeformations.Count > 0)
+        {
+          _.UpdateDeformations(deltaTime);
+        }
+
+        if (_.ParticleEmitters != null)
+        {
+          for (int i = 0; i < _.ParticleEmitters.Length; i++)
+          {
+            if (_.ParticleEmitterTriggers[i] != null && !_.ParticleEmitterTriggers[i].IsTriggered) { continue; }
+            Vector2 emitterPos = _.LocalToWorld(_.Prefab.EmitterPositions[i]);
+            _.ParticleEmitters[i].Emit(deltaTime, emitterPos, hullGuess: null,
+                angle: _.ParticleEmitters[i].Prefab.Properties.CopyEntityAngle ? -_.CurrentRotation + MathHelper.Pi : 0.0f);
+          }
+        }
+
+        for (int i = 0; i < _.Sounds.Length; i++)
+        {
+          if (_.Sounds[i] == null) { continue; }
+          if (_.SoundTriggers[i] == null || _.SoundTriggers[i].IsTriggered)
+          {
+            RoundSound roundSound = _.Sounds[i];
+            Vector2 soundPos = _.LocalToWorld(new Vector2(_.Prefab.Sounds[i].Position.X, _.Prefab.Sounds[i].Position.Y));
+            if (Vector2.DistanceSquared(new Vector2(GameMain.SoundManager.ListenerPosition.X, GameMain.SoundManager.ListenerPosition.Y), soundPos) <
+                roundSound.Range * roundSound.Range)
+            {
+              if (_.SoundChannels[i] == null || !_.SoundChannels[i].IsPlaying)
+              {
+                _.SoundChannels[i] = roundSound.Sound.Play(roundSound.Volume, roundSound.Range, roundSound.GetRandomFrequencyMultiplier(), soundPos);
+              }
+              _.SoundChannels[i].Position = new Vector3(soundPos.X, soundPos.Y, 0.0f);
+            }
+          }
+          else if (_.SoundChannels[i] != null && _.SoundChannels[i].IsPlaying)
+          {
+            _.SoundChannels[i].FadeOutAndDispose();
+            _.SoundChannels[i] = null;
+          }
+        }
+
+        return false;
+      }
+    }
+  }
+}
---
+++ CSharp/Client/Patches/Capture/Map/Levels/LevelObjects/LevelObjectManager.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Reflection;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+
+using Barotrauma;
+using HarmonyLib;
+
+#if CLIENT
+using Barotrauma.Particles;
+#endif
+using Barotrauma.Networking;
+using FarseerPhysics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Voronoi2;
+using Barotrauma.Extensions;
+
+
+namespace ShowPerfExtensions
+{
+  public partial class Plugin
+  {
+    [ShowperfPatch]
+    public class LevelObjectManagerPatch
+    {
+      public static void Initialize()
+      {
+        harmony.Patch(
+          original: typeof(LevelObjectManager).GetMethod("Update", AccessTools.all),
+          prefix: new HarmonyMethod(typeof(LevelObjectManagerPatch).GetMethod("LevelObjectManager_Update_Replace"))
+        );
+      }
+
+      public static bool LevelObjectManager_Update_Replace(float deltaTime, LevelObjectManager __instance)
+      {
+        LevelObjectManager _ = __instance;
+
+        _.GlobalForceDecreaseTimer += deltaTime;
+        if (_.GlobalForceDecreaseTimer > 1000000.0f)
+        {
+          _.GlobalForceDecreaseTimer = 0.0f;
+        }
+
+        if (_.updateableObjects is not null)
+        {
+          foreach (LevelObject obj in _.updateableObjects)
+          {
+            if (GameMain.NetworkMember is { IsServer: true })
+            {
+              obj.NetworkUpdateTimer -= deltaTime;
+              if (obj.NeedsNetworkSyncing && obj.NetworkUpdateTimer <= 0.0f)
+              {
+                GameMain.NetworkMember.CreateEntityEvent(_, new LevelObjectManager.EventData(obj));
+                obj.NeedsNetworkSyncing = false;
+                obj.NetworkUpdateTimer = NetConfig.LevelObjectUpdateInterval;
+              }
+            }
+            if (obj.Prefab.HideWhenBroken && obj.Health <= 0.0f) { continue; }
+
+            if (obj.Triggers != null)
+            {
+              obj.ActivePrefab = obj.Prefab;
+              for (int i = 0; i < obj.Triggers.Count; i++)
+              {
+                obj.Triggers[i].Update(deltaTime);
+                if (obj.Triggers[i].IsTriggered && obj.Prefab.OverrideProperties[i] != null)
+                {
+                  obj.ActivePrefab = obj.Prefab.OverrideProperties[i];
+                }
+              }
+            }
+
+            if (obj.PhysicsBody != null)
+            {
+              if (obj.Prefab.PhysicsBodyTriggerIndex > -1) { obj.PhysicsBody.Enabled = obj.Triggers[obj.Prefab.PhysicsBodyTriggerIndex].IsTriggered; }
+              /*obj.Position = new Vector3(obj.PhysicsBody.Position, obj.Position.Z);
+              obj.Rotation = -obj.PhysicsBody.Rotation;*/
+            }
+          }
+        }
+
+        _.UpdateProjSpecific(deltaTime);
+
+        return false;
+      }
+    }
+  }
+}
